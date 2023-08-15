c251ed00b66670f00e455bc534df91fd3df95c1e
begonufer <begonufernandez@gmail.com>
2023-08-15 11:21:57 +0000

    notas con fetch

--- src/js/component/BodyList.jsx
+++ src/js/component/BodyList.jsx
@@ -1,17 +1,66 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid'; 
 import Pages from './Pages.jsx'
 import Input from './Input.jsx'
 
 const BodyList = () => {
     const [inputValue, setInputValue] = useState ("");
 	const [todos, setTodos] = useState ([]);
+
+	useEffect(() => {
+		getData();
+	},[]);
+
+	const getData = async () => {
+		const response = await fetch ('https://playground.4geeks.com/apis/fake/todos/user/bego');
+		const data = await response.json();
+		setTodos(data);
+	};
+
+	const addToDo = async () => {
+		if (inputValue.trim() === '') return;
+
+		const newToDo = {
+			done: false,
+			id: uuidv4(),
+			label: inputValue,
+		}
+
+		const updatedTodos = [...todos, newToDo];
+
+		setTodos(updatedTodos);
+
+		const response = await fetch ('https://playground.4geeks.com/apis/fake/todos/user/bego', {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(updatedTodos),
+		})
+		getData();
+		setInputValue('');
+	};
+
 	const handleSubmit = (e)=>{
 		if (e.key === "Enter") {
-			e.preventDefault()
-			setTodos(todos.concat(inputValue));
-			setInputValue("");
+			addToDo();
 		}
-	}
+	};
+
+	const deleteToDo = async (itemId) => {
+		const updatedTodos = todos.filter(todo => todo.id !== itemId);
+
+		await fetch('https://playground.4geeks.com/apis/fake/todos/user/bego', {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(updatedTodos),
+		});
+		
+		getData();
+	};
+
     return (
         <>
             <ul className="container-fluid bg-light shadow-sm p-0" id="Block">
@@ -21,10 +70,10 @@ const BodyList = () => {
 					handleSubmit={handleSubmit}
 					placeholder="What do you need to do?"
 				/>
-					{todos.map((item, index) => (
-						<li className="container d-inline-flex justify-content-between px-5">
-							<p>{item}</p>
-							<i className="fas fa-times p-0 float-right justify-content-end" onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i>
+					{todos.map(todo => (
+						<li key={todo.id} className="container d-inline-flex justify-content-between px-5">
+							<p>{todo.label}</p>
+							<i className="fas fa-times p-0 float-right justify-content-end" onClick={() => deleteToDo(todo.id)}></i>
 						</li>
 					))}
 				<div className="m-0 px-3 pt-2 pb-2" id="itemCounter">{todos.length} item left</div>
